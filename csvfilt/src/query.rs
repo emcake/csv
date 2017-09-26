4eebdd93c71798ce6d93e4e01b3b42780ffc66b3
matt <matt@mattkemp.co.uk>
2017-09-26 22:42:34 +0100

    getting there

--- csvfilt/src/query.rs
+++ csvfilt/src/query.rs
@@ -4,18 +4,17 @@ impl QueryString {
     pub fn new(s:String) -> Self { QueryString(s) }
 }
 
-pub enum Comp {
-    Eq,
-    Lt,
-    Gt
+struct ColumnOp(Box<Fn(&Vec<String>) -> bool>);
+
+impl ColumnOp {
+    fn to_fn(self) -> Box<Fn(&Vec<String>) -> bool>
+    {
+        self.0
+    }
 }
 
 enum Query {
-    Op {
-        a : String,
-        comparator : Comp,
-        b : String
-    },
+    Op(ColumnOp),
     And {
         q1 : Box<Query>,
         q2 : Box<Query>
@@ -32,7 +31,7 @@ impl Query {
     pub fn from_qstring(qs:&QueryString) -> Result<Box<Query>, Box<Error>>
     {
         // TODO
-        Ok(Box::new(Query::ColumnOp {index : 0, comparator : Comp::Eq, value : String::default()})) // dummy
+        Ok(Box::new(Query::Op(ColumnOp(Box::new(|row|{row[0] == "VOD.L"}))))) // dummy
     }
 }
 
@@ -41,10 +40,45 @@ pub struct QueryFn (Box<Fn(&Vec<String>) -> bool>);
 use schema::Schema;
 
 impl QueryFn {
-    fn from_query(q:Query, s:&Schema) -> Result<QueryFn, Box<Error>> 
+    fn from_query_inner(q:Query, s:&Schema) -> Result<Box<Fn(&Vec<String>) -> bool>, Box<Error>> 
     {
-        Ok(QueryFn(Box::new(|row|{ true })))
+        /*
+        fn const_const_eq(a:String, b:String) -> Box<Fn(&Vec<String>) -> bool> {
+            let eq = a == b;
+            Box::new(move |row|{eq})
+        };
+        fn const_look_eq(con:String, look:usize) -> Box<Fn(&Vec<String>) -> bool> {
+            Box::new(move |row|{row[look] == con})
+        };
+        fn look_look_eq(look1:usize, look2:usize) -> Box<Fn(&Vec<String>) -> bool> {
+            Box::new(move |row|{row[look1] == row[look2]})
+        };
+        */
+
+        match q {
+            Query::And {q1, q2} =>
+                {
+                    let a = QueryFn::from_query_inner(*q1, s)?;
+                    let b = QueryFn::from_query_inner(*q2, s)?;
+                    Ok(Box::new(move |row|{a(row) && b(row)}))
+                },
+            Query::Or {q1, q2} =>
+                {
+                    let a = QueryFn::from_query_inner(*q1, s)?;
+                    let b = QueryFn::from_query_inner(*q2, s)?;
+                    Ok(Box::new(move |row|{a(row) || b(row)}))
+                },
+            Query::Op(col_op) =>
+                {
+                    Ok(col_op.to_fn())
+                }
+        }
     }
+
+    fn from_query(q:Query, s:&Schema) -> Result<QueryFn, Box<Error>>{
+        let inner = QueryFn::from_query_inner(q, s)?;
+        Ok(QueryFn(inner))
+    } 
 
     pub fn matches(&self, row:&Vec<String>) -> bool {
         self.0(row)
--- csvfilt/src/schema.rs
+++ csvfilt/src/schema.rs
@@ -1,7 +1,28 @@
-pub struct Schema (());
+pub enum ColType {
+    TString,
+    TInt,
+    TFloat,
+    TBool
+}
+
+pub struct ColItem {
+    name : String,
+    col_type : ColType
+}
+
+pub struct Schema (Vec<ColItem>);
 
 impl Schema {
     pub fn from_header(header:&Vec<String>) -> Schema {
-        Schema(())
+        Schema(vec![
+            ColItem { name : "stock".to_owned(), col_type : ColType::TString },
+            ColItem { name : "price".to_owned(), col_type : ColType::TFloat },
+            ColItem { name : "size".to_owned(), col_type : ColType::TInt },
+            ColItem { name : "executed".to_owned(), col_type : ColType::TBool }
+        ])
+    }
+
+    pub fn try_find_col(&self, name:String) -> Option<(usize, &ColItem)> {
+        self.0.iter().enumerate().find(|&x|{x.1.name == name})
     }
 }
